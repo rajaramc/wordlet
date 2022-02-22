4a9d2bd1d32f296942f73d4104a4864850616bf3
Raja Ramachandran <raja@Rajas-Mac-mini.local>
2022-02-22 12:40:31 -0600

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from "./Header"
+import AttemptList from "./AttemptList"
+
+function App() {
+
+  return (
+    <div className='ui center container'>Hello, World</div>
+  );
+}
+
+export default App;
